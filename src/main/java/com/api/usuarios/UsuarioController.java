cbd043735ab2d78cc97ddf5972f29ff8b20c1a75
contato@trilhacoder.com.br <contato@trilhacoder.com.br>
2023-07-06 14:09:21 -0300

    finalizando as 4 operacoes da api

--- src/main/java/com/api/usuarios/UsuarioController.java
+++ src/main/java/com/api/usuarios/UsuarioController.java
@@ -1,9 +1,17 @@
 package com.api.usuarios;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +26,30 @@ public class UsuarioController {
     @PostMapping
     public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listar() {
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.findAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizar(@PathVariable UUID id, @RequestBody Usuario usuario) {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+        if (usuarioOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuario));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Usuario> remover(@PathVariable UUID id) {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+        if (usuarioOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        usuarioRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioOptional.get());
     }
     
 }
